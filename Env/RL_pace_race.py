9697439950864345262e8673c7376992189cab53
FinnLor <88094948+FinnLor@users.noreply.github.com>
2022-01-22 19:58:36 +0100

    Clean up and updated logging (#35)
    * Clean up and updated logging
    
    * SENS_SCALE optional in env_PaceRace
    
    * added global counter
    
    * added global counter

--- Env/RL_pace_race.py
+++ Env/RL_pace_race.py
@@ -1,29 +1,23 @@
-import os
-# import numpy as np
+
 from env_PaceRace import PaceRaceEnv
 from stable_baselines3 import SAC, A2C
-from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback, CallbackList, BaseCallback, EveryNTimesteps
 from stable_baselines3.common.env_checker import check_env
-# from stable_baselines3.common.vec_env import DummyVecEnv, VecCheckNan
-from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
-#from plotting import plot_returns
-
-from LogTraining import CustomTrainingLogCallback, load_Log # Logging
+from LogTraining import CustomTrainingLogCallback, load_Log 
 
 ### CONFIGURATION
-config = {'total_timesteps': 40000,
-          'learning_rate': 3E-3,
-          'n_eval_episodes': 20,
-          'eval_freq': 5000,
-          'checkpoint_freq': 5000}
+config = {'total_timesteps': 100_000,
+          'log_keys': ('obs', 'act'),                   # custom Logger
+          'log_freq_epoch': 10,                         # custom Logger
+          'log_freq_step': 1,                           # custom Logger
+          'monitor_log_path': 'TrainLog/DefaultLog',    # integrated Logger
+          'save_path_models': "models//sac_pace_race"}
 
 
 ### ENV SETUP
-env = Monitor(PaceRaceEnv())
+env = Monitor(PaceRaceEnv(verbose = 2), filename=config['monitor_log_path'])
 check_env(env, warn=True)
-# env = DummyVecEnv([lambda: PaceRaceEnv()])
-# env = VecCheckNan(env, raise_exception=True)   
+
 
 ### MODEL SETUP
 model = SAC("MlpPolicy",
@@ -31,55 +25,17 @@ model = SAC("MlpPolicy",
             verbose=1,
             )
 
-# ### EVAL INIT MODEL
-# print('Random Agent, before training')
-# mean_reward, std_reward = evaluate_policy(model,
-#                                           env,
-#                                           n_eval_episodes=config['n_eval_episodes'],
-#                                           deterministic=True,
-#                                           render=True)
-# print(f'mean_reward={mean_reward:.2f} +/- {std_reward}')
-
-### CALLBACKS
-eval_callback = EvalCallback(eval_env=env,
-                              best_model_save_path="best",
-                              eval_freq=config['eval_freq'],
-                              n_eval_episodes=config['n_eval_episodes'],
-                              deterministic=True,
-                              render=False)
-
-checkpoint_callback = CheckpointCallback(save_freq=config['checkpoint_freq'],
-                                         save_path=os.path.join('best', 'checkpoints'),
-                                         name_prefix='rl_model')
-
-
-
-# callbacks = CallbackList([checkpoint_callback, eval_callback])
-# callback = SaveOnBestTrainingRewardCallback(check_freq=2000, log_dir=log_dir)
-
 ### TRAINING
 print('Start training')
-model.learn(100_000, callback=CustomTrainingLogCallback(info_keywords = ('obs', 'act'), log_freq_epoch=1_000, log_freq_step=1))
-# model.learn(total_timesteps=config['total_timesteps'],
-#             callback=[eval_callback, checkpoint_callback])
+callback=CustomTrainingLogCallback(info_keywords = config['log_keys'], 
+                                   log_freq_epoch=config['log_freq_epoch'], 
+                                   log_freq_step=config['log_freq_step'])
+model.learn(config['total_timesteps'], callback=callback)
 print('End of training')
 
 env.close()
-model.save("models//sac_pace_race") # save model like this
+model.save(config['save_path_models'])
 
 
 # Load Log
-Log = load_Log('TrainLog/myLog')
-
-''' 
-model.save("a2c_pace_race") # save model like this
-model = A2C.load("a2c_pace_race") # load model like this
-# test model like this:
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    if done:
-        break
-    env.render()
-'''
+#Log = load_Log('TrainLog/myLog')
