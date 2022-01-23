e551c74f61c0792a14c8705eed5713a3877efbb7
Finn Lorenzen <lorenzen.finn@gmail.com>
2022-01-23 13:00:56 +0100

    dummy

--- Env/cls_Road.py
+++ Env/cls_Road.py
@@ -89,16 +89,13 @@ class Road:
         
         # Custom road
         else:    
-            # Check inputs for custom road
-            # comment because "AttributeError: 'numpy.ndarray' object has no attribute 'type'"
-            # assert (custom_center_line.type==np.ndarray and 
-            #         custom_center_line.ndim == 2 and
-            #         custom_center_line.shape[1] == 2) , "custom_center_line has to be [nx2] np.ndarray!"
-            
+            # Check inputs for custom road         
             assert (isinstance(custom_center_line,np.ndarray) and
                     custom_center_line.ndim == 2 and
                     custom_center_line.shape[1] == 2) , "custom_center_line has to be [nx2] np.ndarray!"
 
+            f = interp1d(custom_center_line[:,0], custom_center_line[:,1], kind='quadratic')
+            x = 
             line_data = custom_center_line
         
         # Assign properties
