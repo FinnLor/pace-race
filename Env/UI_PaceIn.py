1b783908fc323337bdcbd38fc17382b1162f45cd
emilonia <e.milonia@gmail.com>
2022-01-26 01:15:26 +0100

    Added performance plot and some properties in our_render

--- Env/UI_PaceIn.py
+++ Env/UI_PaceIn.py
@@ -21,8 +21,8 @@ class UIPace:
         self.master = master
         self.canvas_width = 1596
         self.canvas_height = 864 
-        self.road_width_min = 10
-        self.road_width_max = 32
+        self.road_width_min = 8
+        self.road_width_max = 40
         self.road_width = int(np.round(0.5*(self.road_width_min+self.road_width_max)))
         self.width_ok = False
         self.length_ok = False
@@ -33,11 +33,11 @@ class UIPace:
         self.orange = '#ff8844'
         
         # static text
-        self.label_RoadWidthText = tk.Label(self.master, text =f'STRAßENBREITE als ganze Zahl in Meter eingeben. Wertebereich in Meter [{self.road_width_min}-{self.road_width_max}]:')
+        self.label_RoadWidthText = tk.Label(self.master, text =f'STRASSENBREITE als ganze Zahl in Meter eingeben. Wertebereich in Meter [{self.road_width_min}-{self.road_width_max}]:')
         self.label_RoadWidthText.grid(row=0, column=0, sticky = 'W')
         self.label_RoadPath = tk.Label(self.master, text = 'Mindestens zwei PUNKTE als Streckenverlauf erfassen (1 Pixel entspricht 1 Meter):')
         self.label_RoadPath.grid(row=1, column=0,sticky = 'W')
-        self.label_CloseUI = tk.Label(self.master, text = 'Bei gültigen Daten werden diese übernommen und die GUI geschlossen:')
+        self.label_CloseUI = tk.Label(self.master, text = 'Bei gueltigen Daten werden diese uebernommen und die GUI geschlossen:')
         self.label_CloseUI.grid(row=2, column=0,sticky = 'W')
 
         # create entry
@@ -46,21 +46,21 @@ class UIPace:
         self.entry_RoadWidth.insert('end', self.road_width)
         
         # buttons
-        self.button_ApplyRoadWidth = tk.Button(self.master, text = 'übernehmen', command = self.apply_RoadWidth, width = 15)
+        self.button_ApplyRoadWidth = tk.Button(self.master, text = 'uebernehmen', command = self.apply_RoadWidth, width = 15)
         self.button_ApplyRoadWidth.grid(row=0, column=2)
         self.button_ResetRoadPath= tk.Button(self.master, text = 'neu', command = self.reset_RoadPath, width = 10)
         self.button_ResetRoadPath.grid(row=1, column=1)
-        self.button_ApplyRoadPath = tk.Button(self.master, text = 'übernehmen', command = self.apply_RoadPath, width = 15)
+        self.button_ApplyRoadPath = tk.Button(self.master, text = 'uebernehmen', command = self.apply_RoadPath, width = 15)
         self.button_ApplyRoadPath.grid(row=1, column=2)
-        self.button_CloseUI = tk.Button(self.master, text = 'schließen', command = self.closeUI, width = 15)
+        self.button_CloseUI = tk.Button(self.master, text = 'schliessen', command = self.closeUI, width = 15)
         self.button_CloseUI.grid(row=2, column=2)      
         
         # interactive text
-        self.label_RoadWidthCheck = tk.Label(self.master, text = 'ungültig', width=25, background=self.orange)
+        self.label_RoadWidthCheck = tk.Label(self.master, text = 'ungueltig', width=25, background=self.orange)
         self.label_RoadWidthCheck.grid(row=0, column=3, sticky = 'W')
-        self.label_RoadPathCheck = tk.Label(self.master, text = 'ungültig', width=25, background=self.orange)
+        self.label_RoadPathCheck = tk.Label(self.master, text = 'ungueltig', width=25, background=self.orange)
         self.label_RoadPathCheck.grid(row=1, column=3, sticky = 'W')
-        self.label_RoadOkCheck = tk.Label(self.master, text = 'Daten nicht vollständig', width=25, background=self.orange)
+        self.label_RoadOkCheck = tk.Label(self.master, text = 'Daten nicht vollstaendig', width=25, background=self.orange)
         self.label_RoadOkCheck.grid(row=2, column=3, sticky = 'W')
         
         # canvas
@@ -72,7 +72,7 @@ class UIPace:
         self.item_about = tk.Menu(self.item_info)
         self.item_about.add_command(label='About', command=self.ui_about)
         self.item_info.add_cascade(label='Info', menu=self.item_about)
-        master.config(menu=self.item_info)
+        self.master.config(menu=self.item_info) # self.master
 
         # left mouseclick action
         self.canvas_pace.bind('<Button-1>',self.extend_RoadPath)
@@ -85,10 +85,10 @@ class UIPace:
                 self.label_RoadWidthCheck.configure(text ='i.O.', background=self.green)
                 self.width_ok = True
             else:
-                self.label_RoadWidthCheck.configure(text ='ungültig. Vorgaben beachten.', background=self.orange)
+                self.label_RoadWidthCheck.configure(text ='ungueltig. Vorgaben beachten.', background=self.orange)
                 self.width_ok = False
         except:
-            self.label_RoadWidthCheck.configure(text ='ungültig. Vorgaben beachten.', background=self.orange)
+            self.label_RoadWidthCheck.configure(text ='ungueltig. Vorgaben beachten.', background=self.orange)
             self.width_ok = False
         self.set_status()
          
@@ -116,18 +116,18 @@ class UIPace:
                 self.label_RoadPathCheck.configure(text ='i.O.', background=self.green)
                 self.length_ok = True
             else:
-                self.label_RoadPathCheck.configure(text ='ungültig. Vorgaben beachten.', background=self.orange)
+                self.label_RoadPathCheck.configure(text ='ungueltig. Vorgaben beachten.', background=self.orange)
                 self.length_ok = False
         except:
-            self.label_RoadPathCheck.configure(text ='ungültig. Vorgaben beachten.', background=self.orange) 
+            self.label_RoadPathCheck.configure(text ='ungueltig. Vorgaben beachten.', background=self.orange) 
             self.length_ok = False
         self.set_status()
             
     def set_status(self):
         if self.length_ok == False or self.width_ok == False:
-            self.label_RoadOkCheck.configure(text = 'Daten nicht vollständig', background=self.orange)
+            self.label_RoadOkCheck.configure(text = 'Daten nicht vollstaendig', background=self.orange)
         else:
-            self.label_RoadOkCheck.configure(text = 'Daten vollständig', background=self.green)
+            self.label_RoadOkCheck.configure(text = 'Daten vollstaendig', background=self.green)
             
     def ui_about(self):
         ui_about = tk.Tk()
@@ -152,15 +152,13 @@ class UIPace:
                 # test road with a model
                 env1 = PaceRaceEnv(P=1000, custom_center_line = data, custom_roadwidth=self.road_width)
                 env2 = PaceRaceEnv(P=1000, custom_center_line = data, custom_roadwidth=self.road_width)
-                model1 = SAC.load("models/sac_pace_race_FS_02_210122.zip")
-                model2 = SAC.load("models/sac_pace_race_em01.zip")
-                print('Starting new game.')
+                # model = SAC.load("models/sac_pace_race_bad_01.zip")
+                model1 = SAC.load("models/sac_pace_race_EM_01_230122.zip")
+                model2 = SAC.load("models/sac_pace_race_FS_02_210122.zip")
                 obs1 = env1.reset() # get initial obs
                 obs2 = env2.reset() # get initial obs
                 display1 = Render()
                 display2 = Render()
-                display1.update(env1, done = False)
-                display2.update(env2, done = False)
                 vlon1_max=0
                 vlon2_max=0
                 vsum1=0
@@ -169,8 +167,7 @@ class UIPace:
                 # for i in range(1000):
                 #     print(i)
                 #     env.step((0.3, 0.000))
-                #     display.update(env,False)
-                # display.update(env,True)
+                #     display.update(env,False, info)
                 while True:
                     c+=1
                     # print(c)
@@ -189,13 +186,9 @@ class UIPace:
                     vsum2 = vsum2+obs2[0]    
                     print(f'vmax car1={vlon1_max} vmax car2={vlon2_max}')
                     print(f'vsum car1={vsum1} vsum car2={vsum2}')
-                    display1.update(env1, done) # render that current obs
-                    display2.update(env2, done) # render that current obs
+                    display1.update(env1, done, info1, color='blue') # render that current obs
+                    display2.update(env2, done, info2, color='red') # render that current obs
                     
-
-                    
-
-
 
 # create ui_pace
 def main(): 
@@ -206,7 +199,8 @@ def main():
     app = UIPace(ui_pace)   
     ui_pace.mainloop()
     
-
 if __name__ == '__main__':
     main()
+    
+
     
