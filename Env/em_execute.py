54e3afc802cd589db03f7f89006d283b6398e3f9
emilonia <96062884+emilonia@users.noreply.github.com>
2022-01-29 15:21:31 +0100

    Added performance plot and some properties in our_render (#40)
    * Added performance plot and some properties in our_render
    
    * our_render ok-button remains active, added OKS-splash in UI_PaceIn

--- Env/em_execute.py
+++ Env/em_execute.py
@@ -1,7 +1,4 @@
 
-
-
-
 from env_PaceRace import PaceRaceEnv
 from our_render import Render
 from stable_baselines3 import SAC
@@ -11,17 +8,17 @@ import numpy as np
 env = PaceRaceEnv()
 done = False
 i=0
-# model = SAC.load("models/sac_pace_race_FS_02_210122.zip")
-model = SAC.load("models/sac_pace_race_em01.zip")
+# model = SAC.load("models/sac_pace_race_bad_01.zip")
+model = SAC.load("models/sac_pace_race_FS_02_210122.zip")
+# model = SAC.load("models/sac_pace_race_EM_01_230122.zip")
 
 obs = env.reset() # get initial obs
 display = Render()
-display.update(env, done)
-while True:
+while done == False:
     i+=1
     # action = env.action_space.sample() # random
     # obs, reward, done, info = env.step((0.001,0.1)) # manual
     action, _state = model.predict(obs) # agent, get next action from last obs
     obs, reward, done, info = env.step(action) # input action, get next obs
-    print(f'STEP: {i}')
-    display.update(env, done) # render that current obs
+    # print(f'STEP: {i}')
+    display.update(env, done, info, plot_performance=True) # render that current obs
